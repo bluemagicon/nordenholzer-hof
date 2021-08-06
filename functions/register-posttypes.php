3aae9a0bd094e3d05c416624f4dd9ff8316e4582
peverts <phillip@deux.de>
2021-08-06 16:50:01 +0200

    Post Type für Adressen erstellen

--- functions/register-posttypes.php
+++ functions/register-posttypes.php
@@ -48,6 +48,51 @@ function baw_register_my_cpts() {
 	register_post_type( "team", $args );
 
 	/**
+	 * Post Type: Adressen
+	 */
+
+	$labels = [
+		"name" => __( "Adressen", "baw" ),
+		"singular_name" => __( "Adresse", "baw" ),
+		"menu_name" => __( "Adressen", "baw" ),
+		"all_items" => __( "Alle Adressen", "baw" ),
+		"add_new" => __( "Neue Adresse ", "baw" ),
+		"add_new_item" => __( "Neue Adresse", "baw" ),
+		"edit_item" => __( "Adresse bearbeiten", "baw" ),
+		"new_item" => __( "Neue Adresse", "baw" ),
+		"view_item" => __( "Adresse ansehen", "baw" ),
+		"view_items" => __( "Adresse ansehen", "baw" ),
+	];
+
+	$args = [
+		"label" => __( "Adressen", "baw" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => false,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "adresse", "with_front" => false ],
+		"query_var" => true,
+		"menu_position" => 21,
+		"menu_icon" => "dashicons-location-alt",
+		"supports" => [ "title", "thumbnail"],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "adressen", $args );
+
+	/**
 	 * Post Type: Bewertungen.
 	 */
 
