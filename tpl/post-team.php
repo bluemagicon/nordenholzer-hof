185ce172ccce1f479c5b95c72a78eb5a2c22084b
peverts <phillip@deux.de>
2021-10-01 13:58:17 +0200

    Update post-team.php

--- tpl/post-team.php
+++ tpl/post-team.php
@@ -19,15 +19,17 @@ $hide_links = true;
 <article <?php post_class('post post--box'); ?> id="post-<?php the_ID(); ?>">
 	<div class="post-inner">
 
-		<?php if(! $hide_images) { ?>
+		<?php if(!$hide_images) { ?>
 			<?php include 'post/thumbnail.php'; ?>
 		<?php } ?>
 		<div class="post-content">
             <?php if(have_rows('team_daten', $theID)) : the_row(); ?>
                 <span class="is-style-subheadline has-primary-color has-text-color"><?= get_sub_field('position'); ?></span>
                 <h3 class="post-title"><?= get_sub_field('vorname'); ?> <?= get_sub_field('nachname'); ?></h3>
-                <a class="txt-link" href="tel:<?= get_sub_field('telefon'); ?>"><strong><?= get_sub_field('telefon'); ?></strong></a>
-                <a class="txt-link" href="mailto:<?= get_sub_field('email'); ?>"><?= get_sub_field('email'); ?></a>
+                <?php if(!$hide_descr) { ?>
+                    <a class="txt-link" href="tel:<?= get_sub_field('telefon'); ?>"><strong><?= get_sub_field('telefon'); ?></strong></a>
+                    <a class="txt-link" href="mailto:<?= get_sub_field('email'); ?>"><?= get_sub_field('email'); ?></a>
+                <?php } ?>
             <?php endif; ?>
 		</div>
 
